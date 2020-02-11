1dca50fd114ba38151149475c312fafbbbd0208a
Joberth Rogers <joberth.rogers18@gmail.com>
2020-02-10 23:50:56 -0300

    Remove all the NaN values from dataset credit card
    Signed-off-by: Joberth Rogers <joberth.rogers18@gmail.com>

--- Machine-Learning-Datascience/pre-processing/pre_process_credit_data.py
+++ Machine-Learning-Datascience/pre-processing/pre_process_credit_data.py
@@ -7,6 +7,7 @@ Created on Mon Feb 10 20:53:46 2020
 """
 
 import pandas as pd
+import numpy as np
 base = pd.read_csv('credit-data.csv')
 
 # informaçoes da base
@@ -15,3 +16,19 @@ base = pd.read_csv('credit-data.csv')
 # atribuindo valores negativos de idade a media das idades 
 # para tirar a inconsitencia
 base.loc[ base.age < 0 ] = base.age[base.age > 0].mean()
+
+#buscando valores Nulos na coluna age
+base.loc[pd.isnull(base['age'])]
+
+
+# separando os atributos previsores e atributos meta
+# previsores e o restultado classificado
+previsors = base.iloc[: ,1:4].values
+meta = base.iloc[: , 4].values
+
+# preencher valores faltantes pelo sktlearn
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
+# atualizando a base com esses atributos ruins descartados
+imputer = imputer.fit(previsors[:, 0:3])
+previsors[:, 0:3] = imputer.transform(previsors[:, 0:3])
