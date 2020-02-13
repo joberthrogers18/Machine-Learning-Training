a0f4197d76f9537ef8e54fd0900575ee6db2fdd7
Joberth Rogers <joberth.rogers18@gmail.com>
2020-02-13 00:30:05 -0400

    Added transform dummy variables
    Scalonable the results to be in the same scale
    Signed-off-by:Joberth Rogers <joberth.rogers18@gmail.com>

--- Machine-Learning-Datascience/pre-processing/pre_process_censu.py
+++ Machine-Learning-Datascience/pre-processing/pre_process_censu.py
@@ -17,16 +17,33 @@ classe = base.iloc[:, 14].values
 # Biblioteca responsavel por transforma string para numeros
 # Já que os algoritimos não lidam bem com string
 # Para cada string ele associa um número correspondente
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
 labelencoder_previsors = LabelEncoder() 
 # labels = labelencoder_previsors.fit_transform(previsors[:, 1]) // teste de mudança de string para numero
 
+values_categoricals = [1, 3, 5, 6, 7, 8, 9, 13]
 # Transformando as features que tem string em número e guardando em previsores
-previsors[:, 1] = labelencoder_previsors.fit_transform(previsors[:, 1])
-previsors[:, 3] = labelencoder_previsors.fit_transform(previsors[:, 3])
-previsors[:, 5] = labelencoder_previsors.fit_transform(previsors[:, 5])
-previsors[:, 6] = labelencoder_previsors.fit_transform(previsors[:, 6])
-previsors[:, 7] = labelencoder_previsors.fit_transform(previsors[:, 7])
-previsors[:, 8] = labelencoder_previsors.fit_transform(previsors[:, 8])
-previsors[:, 9] = labelencoder_previsors.fit_transform(previsors[:, 9])
-previsors[:, 13] = labelencoder_previsors.fit_transform(previsors[:, 13])
+
+for i in values_categoricals:
+    previsors[:, i] = labelencoder_previsors.fit_transform(previsors[:, i])
+
+# Usando estratégia "dummy" para separar vaiaveis que não tem carater ordinal 
+# em mais variaveis com cada um dos conteúdos, informando se contém ou não
+# um determinado atributo na tabela
+# Ex: white | Black | Calcasian
+#      0        1       0
+#      1        1       1 
+# previsors = base.iloc[:, 8:9].values
+# previsors[:, 0] = labelencoder_previsors.fit_transform(previsors[:, 0])   
+onehotencorder = ColumnTransformer(transformers=[("OneHot", OneHotEncoder(), [1,3,5,6,7,8,9,13])],remainder='passthrough')
+previsors = onehotencorder.fit_transform(previsors).toarray()
+
+# fazendo a mesma coisa para classe
+labelencorder_classe = LabelEncoder()
+classe = labelencorder_classe.fit_transform(classe)
+
+# nomarlizando os valores
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+previsors = scaler.fit_transform(previsors)
