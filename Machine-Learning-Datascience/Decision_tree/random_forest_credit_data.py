ae20bc7bae3b9b5a01981130b417b129075f6edc
Joberth Rogers <joberth.rogers18@gmail.com>
2020-03-03 20:59:41 -0300

    Added random forest algoritm studies
    Signed-off-by: Joberth Rogers <joberth.rogers18@gmail.com>

---
+++ Machine-Learning-Datascience/Decision_tree/random_forest_credit_data.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+
+# Trocando idade negativa pela media 40,92
+base = pd.read_csv('../pre-processing/credit-data.csv')
+base.loc[base.age < 0, 'age'] = 40.92
+               
+# pegando previsores
+previsores = base.iloc[:, 1:4].values
+classe = base.iloc[:, 4].values
+
+# Tirando atributos NaN e substituindo pela media
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
+imputer = imputer.fit(previsores[:, 1:4])
+previsores[:, 1:4] = imputer.transform(previsores[:, 1:4])
+
+# escalonano valores
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+previsores = scaler.fit_transform(previsores)
+
+# dividir em base de treino e teste
+from sklearn.model_selection import train_test_split
+previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.25, random_state=0)
+
+#importanto o algoritmo de arvores aleatórias
+from sklearn.ensemble import RandomForestClassifier
+# random state mantém o valor da precisão constante, n_estimator é quantas arvores vai ser geradas
+classificador = RandomForestClassifier(n_estimators=40, criterion='entropy', random_state=0)
+classificador.fit(previsores_treinamento, classe_treinamento)
+previsoes = classificador.predict(previsores_teste)
+
+# avaliando o algoritimo
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+precisao = accuracy_score(classe_teste, previsoes)
+matriz = confusion_matrix(classe_teste, previsoes)
+
+# 40 árvores -----> 98.4% de acuracia
